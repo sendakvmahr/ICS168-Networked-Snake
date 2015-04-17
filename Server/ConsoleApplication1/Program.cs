ee6853cd30b98603218c465fcccc7c18be90cd73
vivianmahr <vivianmahr@gmail.com>
2015-04-17 15:35:02 -0700

    Start of server, SQLite database

---
+++ Server/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        SQLiteConnection SQLite_connection;
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+        }
+
+        public Program()
+        {
+            System.Console.WriteLine("I am here1");
+            loadDatabase();
+            System.Console.WriteLine("I am here2");
+
+            string sql = "select * from user";
+            SQLiteCommand command = new SQLiteCommand(sql, SQLite_connection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine("User: " + reader["name"] + "\tPw: " + reader["password"] + "\tGames: " + reader["games"] + "\tWins: " + reader["win"]);
+            System.Console.ReadLine();
+
+        }
+
+        void loadDatabase()
+        {
+            SQLite_connection = new SQLiteConnection("Data Source=users.sqlite;Version=3;");
+            SQLite_connection.Open();
+        }
+    }
+}
